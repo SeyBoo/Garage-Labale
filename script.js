28670695d5bf1dc3d1c9e2a7369c2edfa6c0e8ff
Antoine CHEVALIER <antoinechevalier@protonmail.com>
2021-07-12 21:47:05 +0200

    Alpha 1 js height change

--- script.js
+++ script.js
@@ -23,9 +23,9 @@ function menuToggleOff() {
     document.querySelector(".nav-close-bar-1").style.transition = "transform 1s";
     document.querySelector(".nav-close-bar-2").style.transition = "transform 1s";
     setTimeout(function() {
+        document.querySelector(".nav-menu-bar").style.height = "1px";
         document.querySelector(".nav-menu-bar-1").style.marginBottom = ".7rem";
         document.querySelector(".nav-menu-bar-2").style.marginTop = ".7rem";
-        document.querySelector(".nav-menu-bar").style.height = "1px";
     }, 500);
 
 }
