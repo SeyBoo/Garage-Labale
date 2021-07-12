22751f9ff723bf3aedcd108467565a27e699733c
Antoine CHEVALIER <antoinechevalier@protonmail.com>
2021-07-12 21:48:32 +0200

    update js

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ function menuToggleOn() {
     document.querySelector(".nav-menu-bar-1").style.marginBottom = "0rem";
     document.querySelector(".nav-menu-bar-2").style.marginTop = "0rem";
     document.querySelector(".nav-menu-bar").style.height = ".05px";
-    document.querySelector(".nav-menu-bar-1").style.transition = "all 1.5s";
+    document.querySelector(".nav-menu-bar-1").style.transition = "all 1s";
     setTimeout(function() {
         document.querySelector(".menu-mobile").style.width = "100%";
     }, 500);
     
     setTimeout(function() {
         document.querySelector(".nav-close-bar-1").style.transform = "rotate(45deg)";
-        document.querySelector(".nav-close-bar-1").style.transition = "transform 1.5s";
+        document.querySelector(".nav-close-bar-1").style.transition = "transform 1s";
         document.querySelector(".nav-close-bar-2").style.transform = "rotate(90deg)";
-        document.querySelector(".nav-close-bar-2").style.transition = "transform 1.5s";
+        document.querySelector(".nav-close-bar-2").style.transition = "transform 1s";
     }, 500);
 }
 
