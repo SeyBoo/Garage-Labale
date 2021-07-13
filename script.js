950f8bdfe6ccf0fe78decb626e6927b56be09364
Antoine CHEVALIER <antoinechevalier@protonmail.com>
2021-07-13 10:00:57 +0200

    Alpha 1.6  update of the header

--- script.js
+++ script.js
@@ -20,38 +20,14 @@ function menuToggleOff() {
     document.querySelector(".menu-mobile").style.width = "0";
     document.querySelector(".nav-close-bar-1").style.transform = "none";
     document.querySelector(".nav-close-bar-2").style.transform = "none";
-    document.querySelector(".nav-close-bar-1").style.transition = "transform 1s";
-    document.querySelector(".nav-close-bar-2").style.transition = "transform 1s";
     setTimeout(function() {
-        document.querySelector(".nav-menu-bar").style.height = "1px";
         document.querySelector(".nav-menu-bar-1").style.marginBottom = ".7rem";
         document.querySelector(".nav-menu-bar-2").style.marginTop = ".7rem";
-    }, 500);
+        document.querySelector(".nav-menu-bar").style.height = "1px";
+    }, 1000);
 
 }
 
-/*
-document.getElementById("test").addEventListener("click", function() {
-    var element = document.querySelector(".nav-links");
-
-    if (window.getComputedStyle(element).width === "0px") {
-        element.style.width = "100%";
-        console.log("go to block");
-    } else if (window.getComputedStyle(element).width === "100%") {
-        element.style.width = "0px";
-        console.log("go to none");
-    }
-  });
- */
-/*function menuToggle() {
-
-    var element = document.querySelector(".nav-links");
-
-    if (window.getComputedStyle(element).width === "0px") {
-        element.style.width = "100%";
-        console.log("go to block");
-    } else if (window.getComputedStyle(element).width === "100%") {
-        element.style.width = "0px";
-        console.log("go to none");
-    }
-};*/
+function showRest() {
+    document.querySelector("header").style.display = "none";
+}
